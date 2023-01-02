a753dc5f20db63826555e3cdad2f608d2943364e
Darmawan Suka Prajadiputra <114473336+prajadiputra22@users.noreply.github.com>
2023-01-02 22:02:49 +0700

    Update bangun_datar.py

--- tugas/bangun_datar.py
+++ tugas/bangun_datar.py
@@ -1,51 +1,33 @@
 pi = 22/7
 
+#LUAS PERSEGI
 def luas_persegi(sisi=0):
-    """
-    Menghitung Luas persegi
-    """
     return (sisi*sisi)
 
+#LUAS PERSEGI PANJANG
 def luas_persegi_panjang(panjang=0,lebar=0):
-    """
-    Menghitung Luas Persegi Panjang
-    """
     return (panjang*lebar)
 
+#LUAS SEGITIGA
 def luas_segitiga(alas=0,tinggi=0):
-    """
-    Menghitung Luas Segitiga
-    """
     return (alas*tinggi/2)
 
+#LUAS LINGKARAN
 def luas_lingkaran(ruas=0):
-    """
-    Menghitung Luas Lingkaran
-    """
     return (pi*(ruas**2))
 
+#LUAS JAJARGENJANG
 def luas_jajargenjang(alas=0,tinggi=0):
-    """
-    Menghitung Luas Jajar Genjang
-    """
     return (alas*tinggi)
 
+#LUAS BELAH KETUPAT
 def luas_belahketupat(diagonal1=0,diagonal2=0):
-    """
-    Menghitung Luas Belah Ketupat
-    """
     return ((diagonal1*diagonal2)/2)
 
-
+#LUAS LAYANG-LAYANG
 def luas_layang_layang(diagonal1=0,diagonal2=0):
-    """
-    Menghitung Luas Layang-Layang
-    """
     return ((diagonal1*diagonal2)/2)
 
-
+#LUAS TRAPESIUM
 def luas_trapesium(sisi_a=0,sisi_b=0,tinggi=0):
-    """
-    Menghitung Luas Trapesium
-    """
     return (((sisi_a+sisi_b)*tinggi)/2)
